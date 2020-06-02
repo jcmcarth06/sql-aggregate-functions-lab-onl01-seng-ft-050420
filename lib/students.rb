5c941c42c9682d5b313e739e6f9283b91ddc6c5a
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-02 20:48:53 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -2,10 +2,12 @@
 
 def highest_student_gpa
   SELECT MAX(gpa)
-  FROM students;
+  from STUDENTS;
 end
 
 def lowest_student_gpa
+  SELECT MIN(gpa)
+  FROM students;
 end
 
 def average_student_gpa
